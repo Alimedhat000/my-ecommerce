6fcd36a0c0e82b7753aec8742acfb5ac108e72e3
Alimedhat000 <alisensei000@gmail.com>
2025-10-08 22:28:20 +0300

    refactor: move pages into route groups for layout separation

--- apps/frontend/src/app/(store)/layout.tsx
+++ apps/frontend/src/app/(store)/layout.tsx
@@ -1,23 +1,23 @@
-import './globals.css';
+import './../globals.css';
 import localFont from 'next/font/local';
 import Footer from '@/components/layout/footer';
 import HeaderWrapper from '@/components/layout/headerWrapper';
 import { Providers } from './providers';
 
 const oswald = localFont({
-  src: '../fonts/Oswald-VariableFont_wght.ttf',
+  src: '../../fonts/Oswald-VariableFont_wght.ttf',
   variable: '--font-heading',
   display: 'swap',
 });
 
 const robotoCondensed = localFont({
-  src: '../fonts/RobotoCondensed-VariableFont_wght.ttf',
+  src: '../../fonts/RobotoCondensed-VariableFont_wght.ttf',
   variable: '--font-body',
   display: 'swap',
 });
 
 const firaCode = localFont({
-  src: '../fonts/FiraCode-VariableFont_wght.ttf',
+  src: '../../fonts/FiraCode-VariableFont_wght.ttf',
   variable: '--font-mono',
   display: 'swap',
 });
