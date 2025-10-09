07497a0846ce768c4fa3860b18a015988d853a10
Alimedhat000 <alisensei000@gmail.com>
2025-10-09 04:02:35 +0300

    feat: Add Seo Metadata to auth pages and move content to seperate components

---
+++ apps/frontend/src/app/(auth)/auth/_components/loginCard.tsx
@@ -0,0 +1,80 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
+import { FormField } from '../_components/formField';
+import z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const loginSchema = z.object({
+  email: z.string().email('Invalid email address'),
+  password: z.string().min(6, 'Password must be at least 6 characters'),
+});
+
+type LoginFormValues = z.infer<typeof loginSchema>;
+
+export const LoginCard = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginFormValues>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const onSubmit = async (data: LoginFormValues) => {
+    console.log('Form data:', data);
+    // Add your login logic here
+  };
+
+  return (
+    <div className="min-w-[450px] rounded-2xl bg-[#ececec] p-9 shadow-sm">
+      <h1 className="flex justify-center pb-5 focus:ring-0">
+        <Link href="/">
+          <Image
+            src="/logo.svg"
+            alt="Site Logo"
+            width={100}
+            height={30}
+            priority
+          />
+        </Link>
+      </h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col space-y-4"
+      >
+        <FormField
+          id="email"
+          label="Email"
+          type="email"
+          register={register('email')}
+          error={errors.email?.message}
+        />
+        <FormField
+          id="password"
+          label="Password"
+          type="password"
+          register={register('password')}
+          error={errors.password?.message}
+        />
+        <Button
+          type="submit"
+          size={'lg'}
+          className="bg-foreground text-md w-full rounded-lg"
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? 'Signing in...' : 'Sign In'}
+        </Button>
+      </form>
+      <p className="mt-3 text-center text-sm text-gray-600">
+        Don&apos;t have an account?{' '}
+        <Link href="./register" className="underline hover:no-underline">
+          Sign up
+        </Link>
+      </p>
+    </div>
+  );
+};
---
+++ apps/frontend/src/app/(auth)/auth/_components/registerCard.tsx
@@ -0,0 +1,83 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { FormField } from '../_components/formField';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const registerSchema = z.object({
+  name: z.string().min(5, 'Name must be more than 5 characters'),
+  email: z.string().email('Invalid email address'),
+  password: z.string().min(6, 'Password must be at least 6 characters'),
+});
+
+type FormValues = z.infer<typeof registerSchema>;
+
+export const RegisterCard = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormValues>({ resolver: zodResolver(registerSchema) });
+
+  return (
+    <div className="min-w-[450px] rounded-2xl bg-[#ececec] p-9 shadow-sm">
+      <h1 className="flex justify-center pb-5 focus:ring-0">
+        <Link href="/">
+          <Image
+            src="/logo.svg"
+            alt="Site Logo"
+            width={100}
+            height={30}
+            priority
+          />
+        </Link>
+      </h1>
+
+      <form className="flex flex-col space-y-4">
+        <FormField
+          id="name"
+          label="Full Name"
+          type="text"
+          register={register('name')}
+          error={errors.name?.message}
+        />
+        <FormField
+          id="email"
+          label="Email"
+          type="email"
+          register={register('email')}
+          error={errors.email?.message}
+        />
+
+        <FormField
+          id="password"
+          label="Password"
+          type="password"
+          register={register('password')}
+          error={errors.password?.message}
+        />
+
+        <Button
+          type="submit"
+          size={'lg'}
+          className="bg-foreground text-md w-full rounded-lg"
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? 'Registering ...' : 'Register'}
+        </Button>
+      </form>
+
+      <p className="mt-4 text-center text-sm text-gray-600">
+        Already have an account?{' '}
+        <Link href="./login" className="underline hover:no-underline">
+          Sign in
+        </Link>
+      </p>
+    </div>
+  );
+};
